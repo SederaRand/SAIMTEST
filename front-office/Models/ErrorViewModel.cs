b3812a0fca4fb7bf0632c6c72bae765f69ec792f
Sedera Rand <randriamanantenasoasedera@gmail.com>
2022-01-31 10:42:59 +0300

    initialization SAIM Project

---
+++ front-office/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SAIM_FO.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
