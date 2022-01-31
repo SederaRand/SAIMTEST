9d1c5eef082f3b8ef7d8f620ef69e771c9459c8a
Sedera Rand <randriamanantenasoasedera@gmail.com>
2022-01-31 17:47:48 +0300

    sign in features

--- application/WebUI/Controllers/LoginController.cs
+++ application/WebUI/Controllers/LoginController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using SAIM_FO.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +10,36 @@ namespace SAIM_FO.Controllers
 {
     public class LoginController : Controller
     {
+        private IUserService _userService;
+
+        public LoginController(IUserService userService)
+        {
+            this._userService = userService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public void Login(string username, string password)
+        [HttpPost]
+        public ViewResult Login(UserModel user)
         {
+            string username = user.Username;
+            string password = user.Password;
 
+            bool checkResult = _userService.Login(username, password);
+            
+            if (checkResult == true)
+            {
+                // Return Index Home application 
+                return View("~/Views/Customer/Index.cshtml");
+            }
+            else
+            {
+                // Return span to nofity that username or password are wrong
+                return View();                
+            }
         }
     }
 }
