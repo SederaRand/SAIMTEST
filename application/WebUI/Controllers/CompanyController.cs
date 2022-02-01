3ccf79ef37e0099d3ba33c1e30f33b4ed2958c61
Sedera Rand <randriamanantenasoasedera@gmail.com>
2022-02-01 22:50:57 +0300

    add crud contacts

--- application/WebUI/Controllers/CompanyController.cs
+++ application/WebUI/Controllers/CompanyController.cs
@@ -1,9 +1,11 @@
 ﻿using Application.Interfaces;
 using Domain.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SAIM_FO.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -50,8 +52,7 @@ namespace SAIM_FO.Controllers
             {
 
                 return RedirectToAction("Create", "Company");
-            }
-            
+            }            
         }
 
         [HttpPut]
@@ -102,6 +103,88 @@ namespace SAIM_FO.Controllers
                     throw ex;
                 }
             }            
+        }
+
+        /*
+         * Export List of Companies into Txt
+         */
+        public IActionResult ExportTxt()
+        {           
+            try
+            {
+                List<Company> listCompany = _companyService.GetCompanies();
+
+                using (TextWriter tw = new StreamWriter("C:\\Users\\Ibonia\\Desktop\\Params\\companies.txt"))
+                {
+                    foreach (var item in listCompany)
+                    {
+                        tw.WriteLine(string.Format("{0}; {1}; {2}", item.CompanyName, item.Details, item.Address));
+                    }
+                }
+
+                return RedirectToAction("Index", "Company");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        /*
+         * Import Txt list of companies
+         */
+        [HttpPost]
+        public ActionResult ImportTxt(IFormFile file)
+        {
+            Console.WriteLine(file);
+            try
+            {
+                if (file.Length > 0)
+                {
+                    string filePath = @"C:\Users\Public\Documents\";
+
+                    string fileName = Path.GetDirectoryName(file.FileName);
+                    string _path = filePath + fileName;
+
+                    //Create a new list
+                    List<Company> listCompany = new List<Company>(); 
+                    
+                    // Convert the content of list into string
+                    string contents = System.IO.File.ReadAllText(_path);
+
+                    //Execute a loop over the rows.
+                    foreach (string row in contents.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            listCompany.Add(new Company
+                            {                               
+                                CompanyName = row.Split(';')[0],
+                                Details = row.Split(';')[1],
+                                Address = row.Split(';')[2]
+                            });
+                        }
+                    }
+
+                    Console.WriteLine(listCompany.Count);
+                    // Post the list
+                    foreach (var company in listCompany)
+                    {
+                        _companyService.AddCompany(company);
+                    }
+                }
+
+                ViewBag.Message = "Import Successfully!!";
+                return RedirectToAction("Index", "Company");
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+
+                ViewBag.Message = "Import failed!!";
+                return RedirectToAction("Index", "Company");
+            }
         }
     }
 }
--- application/WebUI/Controllers/ContactController.cs
+++ application/WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using SAIM_FO.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,87 @@ namespace SAIM_FO.Controllers
 {
     public class ContactController : Controller
     {
+        private IContactService _contactService;
+        private ICompanyService _companyService;
+
+        public static Dictionary<int, string> listBoxCompanies = new Dictionary<int, string>();
+
+        public ContactController(IContactService contactService, ICompanyService companyService)
+        {
+            this._contactService = contactService;
+            this._companyService = companyService;
+        }
+
         public IActionResult Index()
         {
+            List<Contact> listContact = _contactService.GetContacts();
+            ViewBag.ListContact = listContact;            
+
             return View();
         }
 
         public IActionResult Create()
         {
+            // Take list of companies
+            List<Company> listCompanies = _companyService.GetCompanies();           
+
+            foreach (var company in listCompanies) listBoxCompanies.Add(company.IdCompany, company.CompanyName);
+
+            ViewBag.ListCompanies = listBoxCompanies.Select(x => x.Value);
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ContactModel modelView)
+        {
+            Contact contact = new Contact();
+
+            contact.FirstName = modelView.FirstName;
+            contact.LastName = modelView.LastName;
+            contact.Phone = modelView.Phone;
+
+            if (modelView.Status.ToString() == "Favorites") contact.Status = true;
+            else contact.Status = false;
+
+            string company = modelView.Company;
+            var id = listBoxCompanies.FirstOrDefault(x => x.Value == company).Key;
+
+            contact.CompanyId = id;
+
+            try
+            {
+                _contactService.AddContact(contact);
+
+                return RedirectToAction("Index", "Contact");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Create", "Contact");
+            }
+        }
+
+        public IActionResult Delete(int idContact)
+        {
+            Contact contact = _contactService.GetContact(idContact);
+
+            if (contact == null)
+            {
+                return RedirectToAction("Index", "Contact");
+            }
+            else
+            {
+                try
+                {
+                    _contactService.DeleteContact(contact);
+
+                    return RedirectToAction("Index", "Contact");
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
         }
     }
 }
